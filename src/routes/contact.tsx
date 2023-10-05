2a868454da33719bb541bc602150b9704da00343
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-10-05 13:22:47 -0300

    feat: create contatc page

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -1,22 +1,30 @@
-import { BiLogoGithub, BiLogoGmail, BiLogoLinkedin } from "react-icons/Bi";
+import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
+import { BiLogoGithub, BiLogoLinkedin } from "react-icons/Bi";
 
 export const Contact = () => {
+  const { t } = useTranslation();
+
   return (
-    <main className="bg-gray-200 dark:bg-zinc-900">
-      <section className="flex justify-center items-center py-4 m-auto max-w-7xl h-screen">
-        <div className="text-white bg-zinc-950 p-4 rounded-xl w-1/2 flex flex-col gap-4">
-          <h2 className="text-3xl font-bold">Contato</h2>
-          <p className="w-2/3">
-            Estou sempre à disposição para novos desafios e colaborações empolgantes. Se você está procurando alguém para se juntar à sua equipe, contribuir para um projeto interessante ou
-            simplesmente deseja trocar ideias, fique à vontade para entrar em contato. Tenho paixão pelo que faço e estou comprometido em oferecer resultados excepcionais. Vamos trabalhar juntos para
-            transformar suas ideias em realidade. Estou ansioso para conversar e encontrar maneiras de criar algo incrível juntos!
-          </p>
-          <div className="flex flex-col gap-1">
-            <BiLogoLinkedin size="1.7rem" />
-            <BiLogoGithub size="1.7rem" />
-            <BiLogoGmail size="1.7rem" />
+    <main className="bg-gray-200 dark:bg-zinc-900 darkMode-effect flex-1">
+      <section className="m-auto max-w-4xl px-6 h-full flex flex-col justify-center md:py-10">
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ ease: "easeOut", duration: 1 }}
+          className="text-white bg-zinc-950 p-8 rounded-xl flex flex-col justify-center gap-4"
+        >
+          <h2 className="text-4xl font-bold">{t("contact.title")}</h2>
+          <p className="text-justify text-lg">{t("contact.text")}</p>
+          <div className="flex gap-3 mt-4">
+            <a href="https://www.linkedin.com/in/felipesantosmarcelino/" target="_blank">
+              <BiLogoLinkedin size="1.7rem" />
+            </a>
+            <a href="https://github.com/felipesantos5" target="_blank">
+              <BiLogoGithub size="1.7rem" />
+            </a>
           </div>
-        </div>
+        </motion.div>
       </section>
     </main>
   );
