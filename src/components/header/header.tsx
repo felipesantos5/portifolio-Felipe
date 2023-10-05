3a3728070964c55b422331e8f81e41f2ff8cf742
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-10-05 13:25:43 -0300

    refactor: improved header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,31 +1,25 @@
+import logoWhite from "/logoIcon.png";
+import logoBlack from "/logoIconBlack.png";
+import { MenuHamburguer } from "../menuHamburguer/menuHamburguer";
+import { Nav } from "../navigation/nav";
+import { UseDarkMode } from "../../contexts/DarkModeContext";
 import { Link } from "react-router-dom";
-import logo from "/logoIcon.png";
-import { DarkmodeButton } from "../darkbutton/darkbutton";
-import LanguageButton from "../languageButton/languageButton";
-import { useTranslation } from "react-i18next";
 
 export const Header = () => {
-  const { t } = useTranslation();
+  const { isDarkMode } = UseDarkMode();
 
   return (
-    <header className="bg-gray-200 dark:bg-zinc-900">
-      <section className="flex justify-between items-center py-4 m-auto max-w-7xl ">
-        <img src={logo} alt="logo" className="w-24" />
+    <header className="bg-gray-200 dark:bg-zinc-900 px-8 h-32 darkMode-effect">
+      <section className="flex justify-between items-center py-5 m-auto max-w-7xl ">
+        <Link to={"/"}>
+          <img src={isDarkMode ? logoWhite : logoBlack} alt="logo" className="w-24" />
+        </Link>
         <div className="flex gap-10">
-          <nav className="flex items-center gap-6 text-xl font-semibold text-black dark:text-white">
-            <Link to="/personal">{t("header.whoAmI")}</Link>
-            <Link to="/skills">{t("header.habilities")}</Link>
-            <Link to="/projects">{t("header.projects")}</Link>
-            <Link to="/contact">{t("header.contact")}</Link>
+          <nav className="flex items-center gap-6 text-xl text-black dark:text-white md:hidden">
+            <Nav />
           </nav>
-          <div className="flex items-center gap-3">
-            <DarkmodeButton />
-            <div>
-              <div className="border border-black dark:border-white"></div>
-            </div>
-            <LanguageButton />
-          </div>
         </div>
+        <MenuHamburguer />
       </section>
     </header>
   );
