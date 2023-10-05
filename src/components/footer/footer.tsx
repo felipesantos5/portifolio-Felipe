e54c03bdb1f6020260f290b1b5ffd80cd05fc0d6
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-10-05 13:25:15 -0300

    refactor: improve footer component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,15 +2,15 @@ import { BsGithub, BsLinkedin } from "react-icons/bs";
 
 export const Footer = () => {
   return (
-    <footer className="bg-gray-200 dark:bg-zinc-900">
-      <section className="flex justify-between items-center py-4 m-auto max-w-7xl text-black dark:text-white">
-        <p className="font-semibold">© 2023 Felipe Santos</p>
-        <div className="flex gap-4">
+    <footer className="bg-gray-200 dark:bg-zinc-900 darkMode-effect px-5 h-20 pb-4">
+      <section className="flex justify-between gap-1 items-center flex-wrap m-auto max-w-7xl text-black dark:text-white pt-4 ">
+        <p className="font-sans font-semibold">© 2023 Felipe Santos</p>
+        <div className="flex gap-6">
           <a href="https://www.linkedin.com/in/felipesantosmarcelino/" target="_blank">
-            <BsLinkedin size="1.8rem" />
+            <BsLinkedin size="1.6rem" />
           </a>
           <a href="https://github.com/felipesantos5" target="_blank">
-            <BsGithub size="1.8rem" />
+            <BsGithub size="1.6rem" className="hover:text-zinc-950 dark:hover:text-gray-400" />
           </a>
         </div>
       </section>
