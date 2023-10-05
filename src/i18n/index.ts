998e1953722af7d63627b49411076055148a7f02
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-10-05 13:23:14 -0300

    feat: create i18n config

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -4,12 +4,14 @@ import { initReactI18next } from "react-i18next";
 import enTranlations from "./locales/en/en.json";
 import ptTranlations from "./locales/pt/pt.json";
 
+const storedLanguage = localStorage.getItem("preferredLanguage");
+
 i18n.use(initReactI18next).init({
   resources: {
     en: { ...enTranlations },
     pt: { ...ptTranlations },
   },
-  lng: "pt",
+  lng: storedLanguage || "pt",
   fallbackLng: "pt",
   interpolation: {
     escapeValue: false,
