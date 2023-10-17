eb4a2d211927ad274527083b339f9a509cdcb918
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-10-16 21:28:30 -0300

    feat: add framer motion on the links

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
 
 export const Footer = () => {
@@ -6,12 +7,12 @@ export const Footer = () => {
       <section className="flex justify-between gap-1 items-center flex-wrap m-auto max-w-7xl text-black dark:text-white pt-4 ">
         <p className="font-sans font-semibold">© 2023 Felipe Santos</p>
         <div className="flex gap-6">
-          <a href="https://www.linkedin.com/in/felipesantosmarcelino/" target="_blank">
+          <motion.a href="https://www.linkedin.com/in/felipesantosmarcelino/" target="_blank" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.9 }}>
             <BsLinkedin size="1.6rem" />
-          </a>
-          <a href="https://github.com/felipesantos5" target="_blank">
-            <BsGithub size="1.6rem" className="hover:text-zinc-950 dark:hover:text-gray-400" />
-          </a>
+          </motion.a>
+          <motion.a href="https://github.com/felipesantos5" target="_blank" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.9 }}>
+            <BsGithub size="1.6rem" />
+          </motion.a>
         </div>
       </section>
     </footer>
