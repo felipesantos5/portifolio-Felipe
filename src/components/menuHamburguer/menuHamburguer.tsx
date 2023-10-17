4b3d7a34a2e974b9adf78ebe99fa14cd6f9dbe91
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-10-16 21:28:13 -0300

    style: changing the position of the close button

--- src/components/menuHamburguer/menuHamburguer.tsx
+++ src/components/menuHamburguer/menuHamburguer.tsx
@@ -26,9 +26,9 @@ export const MenuHamburguer = () => {
           transition={{ duration: 0.3 }}
           className="font-semibold text-xl text-black dark:text-white p-4 w-full h-screen flex flex-col gap-6 justify-center items-center fixed top-0 left-0 bg-gray-200 dark:bg-zinc-900 z-10"
         >
-          <div className="fixed top-10 right-8 cursor-pointer text-white text-2xl">
+          <div className="fixed top-10 right-7 cursor-pointer text-white text-2xl">
             <div className=" rounded-full w-10 h-10 flex items-center justify-center">
-              <HiOutlineX size="1.7rem" onClick={menuHamburguerOpen} />
+              <HiOutlineX size="1.8rem" onClick={menuHamburguerOpen} />
             </div>
           </div>
           <Nav func={closeHamburguer} />
