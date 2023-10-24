f6891da3d4551479d9f29d2a99cc15549e429ca6
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-10-24 15:41:52 -0300

    improviment: add a loading component

--- src/routes/skills.tsx
+++ src/routes/skills.tsx
@@ -1,6 +1,10 @@
+import { useEffect, useMemo, useState } from "react";
 import { motion } from "framer-motion";
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { UseDarkMode } from "../contexts/DarkModeContext";
+import Lottie from "lottie-react";
+import loadingAnimationWhite from "../assets/loadingAnimationWhite.json";
+import loadingAnimationBlack from "../assets/loadingAnimationBlack.json";
 
 interface IconTextPair {
   id: string;
@@ -12,23 +16,29 @@ interface IconTextPair {
 export const Skills = () => {
   const [hoveredIcon, setHoveredIcon] = useState<string | null>(null);
   const [lastHoveredIcon, setLastHoveredIcon] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
 
   const { t } = useTranslation();
 
-  const iconTextPairs: IconTextPair[] = [
-    { id: "icon1", icon: "html", text: t("skills.html"), title: "Html" },
-    { id: "icon2", icon: "css", text: t("skills.css"), title: "Css" },
-    { id: "icon3", icon: "js", text: t("skills.javascript"), title: "Javascript" },
-    { id: "icon4", icon: "react", text: t("skills.react"), title: "React" },
-    { id: "icon5", icon: "ts", text: t("skills.typescript"), title: "Type Script" },
-    { id: "icon6", icon: "nextjs", text: t("skills.next"), title: "Next.js" },
-    { id: "icon7", icon: "tailwind", text: t("skills.tailwind"), title: "Tailwind" },
-    { id: "icon8", icon: "sass", text: t("skills.sass"), title: "Sass" },
-    { id: "icon9", icon: "styledcomponents", text: t("skills.styled-components"), title: "Styled Components" },
-    { id: "icon10", icon: "nodejs", text: t("skills.node"), title: "Node.js" },
-    { id: "icon11", icon: "firebase", text: t("skills.firebase"), title: "Firebase" },
-    { id: "icon12", icon: "git", text: t("skills.git"), title: "Git" },
-  ];
+  const { isDarkMode } = UseDarkMode();
+
+  const iconTextPairs: IconTextPair[] = useMemo(
+    () => [
+      { id: "icon1", icon: "html", text: t("skills.html"), title: "Html" },
+      { id: "icon2", icon: "css", text: t("skills.css"), title: "Css" },
+      { id: "icon3", icon: "js", text: t("skills.javascript"), title: "Javascript" },
+      { id: "icon4", icon: "react", text: t("skills.react"), title: "React" },
+      { id: "icon5", icon: "ts", text: t("skills.typescript"), title: "Type Script" },
+      { id: "icon6", icon: "nextjs", text: t("skills.next"), title: "Next.js" },
+      { id: "icon7", icon: "tailwind", text: t("skills.tailwind"), title: "Tailwind" },
+      { id: "icon8", icon: "sass", text: t("skills.sass"), title: "Sass" },
+      { id: "icon9", icon: "styledcomponents", text: t("skills.styled-components"), title: "Styled Components" },
+      { id: "icon10", icon: "nodejs", text: t("skills.node"), title: "Node.js" },
+      { id: "icon11", icon: "firebase", text: t("skills.firebase"), title: "Firebase" },
+      { id: "icon12", icon: "git", text: t("skills.git"), title: "Git" },
+    ],
+    [t]
+  );
 
   const handleMouseEnter = (icon: string) => {
     const textPair = iconTextPairs.find((pair) => pair.icon === icon);
@@ -42,39 +52,63 @@ export const Skills = () => {
     setHoveredIcon(lastHoveredIcon);
   };
 
-  return (
-    <main className="bg-gray-200 dark:bg-zinc-900 darkMode-effect flex-1">
-      <section className="flex justify-between gap-8 md:flex-col m-auto max-w-5xl text-white pt-36 px-6 md:pt-28">
-        <div className="flex justify-start gap-6 flex-wrap w-2/4 md:w-full md:justify-center">
-          {iconTextPairs.map((pair) => (
-            <div key={pair.id}>
-              <motion.img
-                whileHover={{ scale: 1.2 }}
-                whileTap={{ scale: 0.9 }}
-                className="w-20"
-                src={`https://skillicons.dev/icons?i=${pair.icon}`}
-                alt={`icon of ${pair.icon}`}
-                onMouseEnter={() => handleMouseEnter(pair.icon)}
-                onMouseLeave={handleMouseLeave}
-                loading="lazy"
-              />
-            </div>
-          ))}
-          {/* <div className="m-auto">
-              <Lottie className="w-52" animationData={isDarkMode ? loadingAnimationWhite : loadingAnimationBlack} />
-            </div> */}
-        </div>
+  useEffect(() => {
+    const imageLoadPromises = iconTextPairs.map((pair) => {
+      const image = new Image();
+      image.src = `https://skillicons.dev/icons?i=${pair.icon}`;
+      return new Promise<void>((resolve) => {
+        image.onload = () => {
+          resolve();
+        };
+      });
+    });
 
-        <div className="bg-black p-4 text-lg w-1/2 h-96 rounded-xl md:w-full md:h-full md:mb-6">
+    Promise.all(imageLoadPromises).then(() => {
+      setLoading(false);
+    });
+  }, [iconTextPairs]);
+
+  return (
+    <main className="bg-gray-200 dark:bg-zinc-900 darkMode-effect flex-1 flex items-center justify-center">
+      <section className="flex justify-between items-center gap-8 md:flex-col m-auto max-w-5xl text-white px-6">
+        <motion.div className="flex justify-start gap-6 flex-wrap w-2/4 md:w-full md:justify-center">
+          {loading ? (
+            <div className="m-auto">
+              <Lottie className="w-52" animationData={isDarkMode ? loadingAnimationWhite : loadingAnimationBlack} />
+            </div>
+          ) : (
+            iconTextPairs.map((pair) => (
+              <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 2 }} transition={{ ease: "easeOut", duration: 3 }} key={pair.id}>
+                <motion.img
+                  whileHover={{ scale: 1.2 }}
+                  whileTap={{ scale: 0.9 }}
+                  className="w-20"
+                  src={`https://skillicons.dev/icons?i=${pair.icon}`}
+                  alt={`icon of ${pair.icon}`}
+                  onMouseEnter={() => handleMouseEnter(pair.icon)}
+                  onMouseLeave={handleMouseLeave}
+                />
+              </motion.div>
+            ))
+          )}
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ ease: "easeOut", duration: 1 }}
+          className={`bg-black p-4 text-lg w-1/2 h-96 rounded-xl flex justify-center md:w-full  ${!hoveredIcon ? "md:h-44" : "md:h-full"} md:mb-6`}
+        >
           {hoveredIcon ? (
             <div>
               <h2 className="text-xl font-semibold mb-2">{iconTextPairs.find((pair) => pair.text === hoveredIcon)?.title}</h2>
               <p className="">{hoveredIcon}</p>
             </div>
           ) : (
-            <p className="text-center text-zinc-300">{t("skills.card")}</p>
+            <div className="flex items-center justify-center text-center">
+              <p className="text-zinc-300 text-xl">{t("skills.card")}</p>
+            </div>
           )}
-        </div>
+        </motion.div>
       </section>
     </main>
   );
